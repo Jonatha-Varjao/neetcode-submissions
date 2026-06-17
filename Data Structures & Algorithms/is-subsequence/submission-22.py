4e8e0dcae2d45b6968eb54dd89e7caa8322a5d0f
Jonatha Oliveira Reis Varjão <jonathavarjao@gmail.com>
2026-06-17 20:39:16 -0300

    Add: is-subsequence - submission-22

---
+++ Data Structures & Algorithms/is-subsequence/submission-22.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        s_pointer = 0
+        t_pointer = 0
+
+        if s == "":
+            return True
+        if t == "":
+            return False
+        
+        while s_pointer < len(s) and t_pointer < len(t) :            
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer+=1    
+            
+            t_pointer+=1
+
+        return True if s_pointer == len(s) else False
+                
+
+
+        
